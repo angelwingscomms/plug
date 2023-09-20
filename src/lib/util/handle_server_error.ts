956d0db3dddacb244253214bcac2b533ab39728d
EdwardGold Edem Hogan <edge37@outlook.com>
2023-09-20 19:13:56 +0000

    .

--- src/lib/util/handle_server_error.ts
+++ src/lib/util/handle_server_error.ts
@@ -5,9 +5,9 @@ interface CustomError {
 	message: string;
 }
 
-export const handle_server_error = (m: string | Request, e: Error | CustomError) => {
+export const handle_server_error = (m: string | Request, e?: Error | CustomError) => {
 	let r: string
 	if (typeof m === 'string') {r = m} else {r = `${m.method} ${m.url}`}
-	console.error(r, e);
+	console.error(r, e ?? '');
 	return e instanceof Error || e instanceof String ? error(500) : error(e.status ?? 500, e.message ?? undefined);
 };
--- src/routes/edit/+page.server.ts
+++ src/routes/edit/+page.server.ts
@@ -1,12 +1,10 @@
-import { error, redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 import { client } from "$lib/util/redis";
+import { handle_server_error } from "$lib/util/handle_server_error";
 
 export const load: PageServerLoad = async ({ locals }) => {
     const session = await locals.getSession()
-    console.log('es', session)
-    if(!session?.user?.id) throw redirect(303, '/auth')
-    const user = await client.json.get(session.user.id)
-    if (!user) throw error(404);
+    const user = await client.json.get(session?.user?.id);
+    if (!user) throw handle_server_error(`expected to, but did not, find user with ${session?.user?.id}`);
     return user
 }
