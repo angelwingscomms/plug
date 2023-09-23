faf16cb9c06841fa5a0e5a2c4e245ecc78860e05
EdwardGold Edem Hogan <edge37@outlook.com>
2023-09-23 15:46:00 +0000

    embed endpoint

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,3 +1,5 @@
+import { dev } from "$app/environment";
+
 export const items_per_page = 7;
 export const replies_index_name = 'replies';
 export const replied_index_name = 'replied';
@@ -8,8 +10,8 @@ export const embedding_field_name = 'v';
 export const ids_hash = 'ids';
 export const user_id_prefix = 'user_'
 export const user_index = 'users'
-export const embed_endpoint = 'http://127.0.0.1:10000';
-export const feedback_index = "feedback"
+export const embed_endpoint = dev ? 'http://127.0.0.1:10000' : '18.215.251.72:80';
+export const feedback_index = 'feedback';
 export const feedback_id_prefix = "feedback_"
 export const content_attributes = ['name', 'body'];
 export const editable_attributes = ['name', 'body', 'payment', 'allow_replies'];
