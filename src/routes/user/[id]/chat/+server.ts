946edc95f0f9b809eb8f3085860aa39d23728187
Gold Edem Hogan <edge37@outlookcom>
2024-02-23 10:42:29 -1200

    .

--- src/routes/user/[id]/chat/+server.ts
+++ src/routes/user/[id]/chat/+server.ts
@@ -15,7 +15,7 @@ export const POST: RequestHandler = async ({ request, params, locals }) => {
 		};
 		const id = `${message_id_prefix}${await client.incr('last_free_message_id')}`;
 		await client.json.set(id, '$', message);
-		message_channel.publish({ name: message_name(params.id, locals.user), id, value: { ...message } });
+		message_channel.publish(message_name(params.id, locals.user), { id, value: { ...message } });
 		return new Response();
 	} catch (e) {
 		throw handle_server_error(`/chat/${params.id} error: ${e}`, request);
