43a95d60e7600a1a44b620d251a3b92f8c59a491
Gold Edem Hogan <edge37@outlookcom>
2024-03-12 02:06:52 -1200

    .

--- src/lib/util/embedding/oai.ts
+++ src/lib/util/embedding/oai.ts
@@ -1,6 +1,7 @@
+import { embedding_model } from '$lib/constants';
 import { openai } from '$lib/util/openai';
 
 export const oai = (input: string) =>
-	openai.embeddings.create({ model: "text-embedding-3-large", input }).then((r) => 
+	openai.embeddings.create({ model: embedding_model, input }).then((r) => 
 		r.data[0].embedding
 	);
