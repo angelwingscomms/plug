5260925f0f71416136f608e1b71718cf1ed28589
Gold Edem Hogan <edge37@outlookcom>
2024-03-12 07:30:43 -1200

    .

--- src/lib/util/m/unseen/set.ts
+++ src/lib/util/m/unseen/set.ts
@@ -3,5 +3,7 @@ import { unset } from './unset';
 
 export const set = async (u: string, t: string) => {
 	unset(u, t);
+	const r = await client.json.get('um', { path: t });
+	if (!Array.isArray(r)) client.json.set('um', `$.${t}`, []);
 	await client.json.arrAppend('um', `$.${t}`, u);
 };
